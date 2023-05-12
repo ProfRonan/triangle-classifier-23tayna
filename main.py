141e6b5a1467def38ea0bdbce7830e51b2ba5e87
tayna <silvamikaella2301@gmail.com>
2023-05-12 14:21:14 -0300

    terminei

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-a= int(input("lado a= "))
-b= int(input("lado b= "))
-c= int(input("lado c= "))
+a= int(input("lado a="))
+b= int(input("lado b="))
+c= int(input("lado c="))
 
-if (a < b + c and b < a + c and c < b + c):
-    if (a == b and b == c):
-       print("nos lados formam um Triângulo Equilátero.")
-    else:
-        if (a == b or a == c or b == c):
-            print("nos lados formam um Triângulo Isósceles.")
+if (a+b> c and a+c> b and b+c > a):
+    if (a==b and a==c):
+        print("nos lados formam um triângulo Equilátero.")
+    
+    else: 
+        if(a == b or a == c or b == c): 
+            print("nos lados formam um triângulo Isósceles.")
         else:
-            print("ns lados formam um Triângulo Escaleno.")
-else:
-    print("nos lados não formam um triângulo!")
+            print("nos lados formam um triângulo Escaleno.")
+            
+else: 
+    print("Não é um triângulo.")
